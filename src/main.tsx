3b7edea1c5f7c25e02c9efe89a06dc8590a87cdd
Tushar Debnath <txdebnath@gmail.com>
2021-04-26 15:38:39 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -14,8 +14,34 @@ let App = () => {
         <li className={tab ? "tab" : "tab is-active"} onClick={() => Toggle(!tab)}><a>Light Shades</a></li>
       </ul>
     </div>
-    
+    {tab? <div >
+    <div className="field">
+			<div className="label">Enter Base Color (#color-code)</div>
+			<input type="input " className="input " value="#0054A3" id="color" />
+		</div>
+		<div className="field">
+			<div className="label">Number of shades required</div>
+			<input type="input " className="input " value="5" id="count" />
+		</div>
+		<div className="field">
+			<div className="label">Darken by (0.1~0.9)</div>
+			<input type="input" className="input  " value="0.1" id="ratio" />
+		</div>
+
+
+		<div className="field is-grouped">
+			<div className="control">
+				<button className="button is-link" id="create">Generate</button>
+			</div>
+			<div className="control">
+				<button className="button is-link is-light" id="cancel">Cancel</button>
+			</div>
+		</div>
+    </div> : null}
+
+
   </div>
+  
 }
 
 
