fbc06aad91254b77c4de84b5c1b8aec2ad20a648
Tushar Debnath <txdebnath@gmail.com>
2021-04-26 15:17:51 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { useState } from 'react'
 import './bulma.min.css'
 
 
 let App = () => {
+  const [tab, Toggle] = useState(true);
+
   return <div >
     <div className="tabs">
-      <ul id="tabs" data-tabs>
-        <li className="tab is-active" ><a>Dark Shades</a></li>
-        <li className="tab"><a>Light Shades</a></li>
-
+      <ul id="tabs" >
+        <li className={tab ? "tab is-active" : "tab"} onClick={() => Toggle(!tab)}><a>Dark Shades</a></li>
+        <li className={tab ? "tab" : "tab is-active"} onClick={() => Toggle(!tab)}><a>Light Shades</a></li>
       </ul>
     </div>
+    
   </div>
 }
 
